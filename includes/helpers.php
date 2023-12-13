51a63c22b60ec076526b7ceffb1449c2164f939e
Esteban <esteban@attitude.cl>
2023-12-13 15:43:42 -0300

    0.1.4

--- includes/helpers.php
+++ includes/helpers.php
@@ -10,9 +10,60 @@ if (!defined('ABSPATH')) {
  * @since 2023-12-04
  * @return string
  */
-if (!function_exists('hxwp_api_url')) {
-	function hxwp_api_url()
-	{
-		return home_url(HXWP_ENDPOINT . '/' . HXWP_ENDPOINT_VERSION);
+function hxwp_api_url()
+{
+	return apply_filters('hxwp/api_url', home_url(HXWP_ENDPOINT . '/' . HXWP_ENDPOINT_VERSION));
+}
+
+/**
+ * HTMX send header response
+ *
+ * @since 2023-12-13
+ *
+ * @param string $status (success|error)
+ * @param string $message (message)
+ * @param array $data (extra data, optional)
+ * @param array $action (WP action, optional, default value: default)
+ *
+ * @return void
+ */
+function hxwp_send_header_response($status = 'success', $message = '', $data = [], $action = null)
+{
+	if ($action === null) {
+		// Check if action is set inside $_POST['hxparams']['action']
+		$action = isset($_POST['hxparams']['action']) ? sanitize_text_field($_POST['hxparams']['action']) : '';
 	}
+
+	// Action still empty, null or not set?
+	if (empty($action)) {
+		$action = 'default';
+	}
+
+	// If success, set code to 200
+	$code = $status === 'success' ? 200 : 400;
+
+	// Response array
+	$response = [
+		'hxwpResponse:' . $action => [
+			'action'  => $action,
+			'status'  => $status,
+			'message' => $message,
+			'data'    => $data,
+		],
+	];
+
+	// Headers already sent?
+	if (headers_sent()) {
+		wp_die('HWXP Error: Headers already sent.');
+	}
+
+	// Filter our response
+	$response = apply_filters('hxwp/header_response', $response, $action, $status, $message, $data);
+
+	// Send our response
+	status_header($code);
+	nocache_headers();
+	header('HX-Trigger: ' . json_encode($response));
+
+	die(); // Don't need wp_die() here
 }
